0cde86e79c97fdee715a0a89e4dc9065a9233a2f
Seokkitdo <sjd12329@gmail.com>
2022-01-24 17:48:40 +0900

    docs : import index.css 삭제

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import Router from './Router';
 import './styles/common.scss';
 import './styles/constant.scss';
