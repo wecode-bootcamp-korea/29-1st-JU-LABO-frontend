8d15a212f84a0a4212b7c61a5f7e9d4d3a396b8f
Namju Yun <southpoley@gmail.com>
2022-01-25 15:44:24 +0900

    feat : Main 페이지 작업, mainBanner 슬라이더, seasons, brewery 섹션 추가

---
+++ src/pages/Main/MAIN_DATA.js
@@ -0,0 +1,64 @@
+export const SLIDER_DATA = [
+  {
+    image:
+      'https://images.unsplash.com/photo-1591243315780-978fd00ff9db?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
+  },
+  {
+    image:
+      'https://images.unsplash.com/photo-1565824093823-8e6243ac02f9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
+  },
+  {
+    image:
+      'https://images.unsplash.com/photo-1580657274234-7339717f4541?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
+  },
+  {
+    image:
+      'https://images.unsplash.com/photo-1471967183320-ee018f6e114a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2071&q=80',
+  },
+  {
+    image:
+      'https://images.unsplash.com/photo-1590189182193-1fd44f2b4048?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
+  },
+];
+
+export const BREWERY_DATA = [
+  {
+    brewery: 'Gangnam Beer Brewery',
+    location: 'Gangnam, Seoul',
+    image:
+      'https://images.unsplash.com/photo-1559526642-c3f001ea68ee?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1674&q=80',
+    description:
+      'Located in the heart of Seoul, Gangnam Beer Brewery provides quality beer everyday to business people of Gangnam',
+  },
+  {
+    brewery: 'Guinsa Temple',
+    location: 'Danyang, Chungcheongbuk-do',
+    image:
+      'https://images.unsplash.com/photo-1609171150381-6f371e1fb47e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2117&q=80',
+    description: `Makgeolli made with pure spring water of Danyang Mountains. Guinsa Temple presents their famous liquor along with temple stay experiences.`,
+  },
+  {
+    brewery: 'Buseok Winery',
+    location: 'Yeongju, Gyeongsangbuk-do',
+    image:
+      'https://images.unsplash.com/photo-1506377247377-2a5b3b417ebb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
+    description:
+      'Not a lot of people are aware of the fact that Korea too has some fine wineries, manufacturing quality wines.',
+  },
+  {
+    brewery: 'Hallasan Soju Factory',
+    location: 'Hallim, Jeju',
+    image:
+      'https://images.unsplash.com/photo-1530037335614-e68828dcf258?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2071&q=80',
+    description:
+      'All time favorite soju, best served luke-warm, Hallasan soju is famous for a lot of things. But probably the best is its taste and history.',
+  },
+  {
+    brewery: 'Strawberry Cider Chungju',
+    location: 'Chungju, Chungcheongbuk-do',
+    image:
+      'https://images.unsplash.com/photo-1635527726426-cc74da84703e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
+    description:
+      'Who is in for more sweet side of alcohol? Strawberry is just good by itself, but it is better when made into a beverage!',
+  },
+];
--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,7 +1,133 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { SLIDER_DATA, BREWERY_DATA } from './MAIN_DATA';
+import './Main.scss';
 
 const Main = () => {
-  return <div />;
+  const [currSlide, setCurrSlide] = useState(0);
+  const [brewerySelect, setBrewerySelect] = useState(0);
+  const length = SLIDER_DATA.length;
+
+  useEffect(() => {
+    const mainBannerTime = setTimeout(() => {
+      setCurrSlide(currSlide === length - 1 ? 0 : currSlide + 1);
+    }, 5000);
+
+    return () => clearTimeout(mainBannerTime);
+  });
+
+  // const prevSlide = () => {
+  //   setCurrSlide(currSlide === 0 ? length - 1 : currSlide - 1);
+  // };
+
+  const nextSlide = () => {
+    setCurrSlide(currSlide === length - 1 ? 0 : currSlide + 1);
+  };
+
+  const handleBreweryClick = e => {
+    const current = brewerySelect;
+    const next = e.target.getAttribute('index');
+    setBrewerySelect(brewerySelect + (next - current));
+  };
+
+  return (
+    <main className="Main">
+      <section className="MainBanner">
+        {/* <i className="fas fa-arrow-circle-left LeftArrow" onClick={prevSlide} />
+        <i
+          className="fas fa-arrow-circle-right RightArrow"
+          onClick={nextSlide}
+        /> */}
+        {SLIDER_DATA.map((slide, index) => (
+          <div
+            className={index === currSlide ? 'Slide Active' : 'Slide'}
+            key={index}
+          >
+            {index === currSlide && (
+              <img
+                alt="slider"
+                src={slide.image}
+                className="SliderImage"
+                onClick={nextSlide}
+              />
+            )}
+          </div>
+        ))}
+      </section>
+
+      <section className="Seasons">
+        <h1>THEMES</h1>
+        <h2>
+          We categorize our choice of beverages in themes of four seasons.
+        </h2>
+        <div className="SeasonsCardContainer">
+          <Link to="/main" className="SeasonsCard">
+            <div className="SeasonsCardImage Spring" />
+            <h3>SPRING</h3>
+            <h4>Fresh breeze and floral sweetness, easy to drink.</h4>
+          </Link>
+          <Link to="/main" className="SeasonsCard">
+            <div className="SeasonsCardImage Summer" />
+            <h3>SUMMER</h3>
+            <h4>Scents of summer beach, coconut rum and more</h4>
+          </Link>
+          <Link to="/main" className="SeasonsCard">
+            <div className="SeasonsCardImage Autumn" />
+            <h3>AUTUMN</h3>
+            <h4>Cozy gatherings around the bonfire.</h4>
+          </Link>
+          <Link to="/main" className="SeasonsCard">
+            <div className="SeasonsCardImage Winter" />
+            <h3>WINTER</h3>
+            <h4>Warm your body up with vin chaud and vodka.</h4>
+          </Link>
+        </div>
+      </section>
+
+      <section className="Brewery">
+        <h1>BREWERY</h1>
+        <h2>
+          We host various local breweries of Korea, that are manufacturing
+          high-quality beverages with local ingredients. Thanks to the
+          breweries' efforts and hard work, we can provide fresh beverages at
+          all moments.
+        </h2>
+        <article className="BreweryContainer">
+          <ul className="BreweryList">
+            {BREWERY_DATA.map((brewery, index) => {
+              return (
+                <li
+                  className={
+                    index === brewerySelect
+                      ? 'BreweryItem Active'
+                      : 'BreweryItem'
+                  }
+                  key={index}
+                  index={index}
+                  onClick={handleBreweryClick}
+                >
+                  <div className="BreweryTitle">
+                    <h3>{brewery.brewery.toUpperCase()}</h3>
+                    <h4>{brewery.location}</h4>
+                  </div>
+                  <i className="fas fa-chevron-right" />
+                </li>
+              );
+            })}
+          </ul>
+          <div className="BreweryImageWrap">
+            <img
+              className="BreweryImage"
+              src={BREWERY_DATA[brewerySelect].image}
+            />
+            <p className="BreweryDescription">
+              {BREWERY_DATA[brewerySelect].description}
+            </p>
+          </div>
+        </article>
+      </section>
+    </main>
+  );
 };
 
 export default Main;
