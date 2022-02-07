feb0a74544b46e3bae201d8b220a081ba0b367ce
Namju Yun <southpoley@gmail.com>
2022-02-07 11:21:08 +0900

    feat : /products 페이지 내 배너 이미지 사계절에 맞게 연동

--- src/pages/ProductList/CATEGORY_TABLE.js
+++ src/pages/ProductList/CATEGORY_TABLE.js
@@ -24,3 +24,14 @@ export const CATEGORY_TABLE = {
   23: ['Winter', 'Sake'],
   24: ['Winter', 'Makgeolli'],
 };
+
+export const CATEGORY_IMAGE = {
+  Spring:
+    'https://images.unsplash.com/photo-1491591462767-3b91b2a19487?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
+  Summer:
+    'https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2073&q=80',
+  Autumn:
+    'https://images.unsplash.com/photo-1523712999610-f77fbcfc3843?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
+  Winter:
+    'https://images.unsplash.com/photo-1491002052546-bf38f186af56?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2108&q=80',
+};
