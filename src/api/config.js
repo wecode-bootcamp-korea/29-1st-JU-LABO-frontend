3c3bf56033ac4c1c7df7a2db129b1553de733b06
Seokkitdo <sjd12329@gmail.com>
2022-02-09 17:16:40 +0900

    Modify : config url 수정

--- src/api/config.js
+++ src/api/config.js
@@ -1,10 +1,9 @@
-const BASE_URL = 'http://10.58.0.160:8000';
-
+const BASE_URL = 'http://3.90.45.177:8000';
 export const api = {
   fetchLogin: BASE_URL + '/users/login',
   fetchSignup: BASE_URL + '/users/signup',
   fetchProductList: BASE_URL + '/products/productlist',
-  fetchSearchList: BASE_URL + '/products/productsearch',
+  fetchSearchList: BASE_URL + '/categories/productsearch',
   fetchDetailProduct: BASE_URL + '/products',
   fetchAddCart: BASE_URL + '/carts',
   fetchCartItems: BASE_URL + '/carts',
