78cd8d49edbf26948e30a129ffdaa1cf7100e153
Namju Yun <southpoley@gmail.com>
2022-02-10 15:35:54 +0900

    Add : ProductCard 컴포넌트 클릭 시 popular count +1

--- src/api/config.js
+++ src/api/config.js
@@ -2,8 +2,8 @@ const BASE_URL = 'http://3.90.45.177:8000';
 export const api = {
   fetchLogin: BASE_URL + '/users/login',
   fetchSignup: BASE_URL + '/users/signup',
+  fetchPopular: BASE_URL + '/users/popular',
   fetchProductList: BASE_URL + '/products/productlist',
-  fetchSearchList: BASE_URL + '/categories/productsearch',
   fetchDetailProduct: BASE_URL + '/products',
   fetchAddCart: BASE_URL + '/carts',
   fetchCartItems: BASE_URL + '/carts',
