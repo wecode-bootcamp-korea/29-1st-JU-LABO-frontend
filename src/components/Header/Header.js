215cde95415a140cd5396339258d8c05e5333467
Namju Yun <southpoley@gmail.com>
2022-02-10 18:16:00 +0900

    Add : 헤더에서 검색 기능 구현

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,19 +1,32 @@
-import React from 'react';
-import { useLocation } from 'react-router-dom';
+import React, { useState } from 'react';
+import { useLocation, Link, useNavigate } from 'react-router-dom';
 import CartModal from '../../pages/Cart/CartModal/CartModal';
-import './Header.scss';
 import Category from './Category';
 import About from './About';
-import { Link } from 'react-router-dom';
+import './Header.scss';
 
 const Header = ({ setIsOpen, isOpen }) => {
   const token = sessionStorage.getItem('login');
   const username = sessionStorage.getItem('username');
   const location = useLocation();
+  const navigate = useNavigate();
+  const [searchVal, setSearchVal] = useState('');
 
   const openCartModal = () => {
     if (location.pathname !== '/cart') {
       setIsOpen(prev => !prev);
+    }
+  };
+
+  const handleSearchInput = e => {
+    setSearchVal(e.target.value);
+  };
+
+  const handleSearchEnter = e => {
+    if (e.key === 'Enter') {
+      e.preventDefault();
+      navigate(`/search?search=${searchVal}`);
+      setSearchVal('');
     }
   };
 
@@ -34,7 +47,13 @@ const Header = ({ setIsOpen, isOpen }) => {
               <div className="searchIcon">
                 <i className="fas fa-search" />
               </div>
-              <input type="text" placeholder="search for products" />
+              <input
+                type="text"
+                placeholder="search for products"
+                value={searchVal}
+                onChange={handleSearchInput}
+                onKeyPress={handleSearchEnter}
+              />
               <div className="letter">
                 <i className="far fa-envelope" />
               </div>
